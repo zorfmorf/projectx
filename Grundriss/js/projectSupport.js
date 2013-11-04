818c0cfb0c1aba05866791030c2f28dca26a638b
iljaX <ilja_m19@yahoo.de>
2013-11-04 23:43:17 +0100

    noch geiler geworden :)
    u.a. kann man jetzt auch noch beliebige Polygone zeichnen

---
+++ Grundriss/js/projectSupport.js
@@ -0,0 +1,13 @@
+
+function ProjectSupport () {
+      
+  this. get_random_color = function () {
+      function c() {
+        return Math.floor(Math.random()*256).toString(16)
+      }
+  return "#"+c()+c()+c();
+  }
+  
+   
+    
+}
